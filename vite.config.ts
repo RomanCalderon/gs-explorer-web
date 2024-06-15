00033fa702bc7f44c74b3e242f91e7f23376a34b
Roman Calderon <romanc647@gmail.com>
2024-06-15 10:26:04 -0400

    chore: Update Vite configuration for public directory, resolve alias, and build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  }
 })
