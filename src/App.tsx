0ff917f169b018e34b778ddf0d4950c68a2233cb
Roman Calderon <romanc647@gmail.com>
2024-08-05 19:48:28 -0400

    Refactor: add QueryClient + axios to App, refactor Posts.tsx and remove usePaginatedPosts.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import axios from 'axios'
 
 import './App.css'
 import Home from './pages/Home'
 import Layout from './pages/Layout'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: async ({ queryKey: [url] }) => {
+        const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/api/${url}`);
+        return data;
+      },
+      staleTime: 1000 * 60 * 5,
+      retry: 3,
+    },
+  },
+});
+
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route path='/' element={<Home />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Layout />}>
+              <Route path='/' element={<Home />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </>
   )
 }
