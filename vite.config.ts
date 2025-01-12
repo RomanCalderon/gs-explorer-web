82e6d5267745a67c374756d786a6740b0a8bc341
Roman Calderon <romanc647@gmail.com>
2025-01-12 11:11:57 -0500

    Update Vite configuration: replace path import with fileURLToPath for better compatibility with ES modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+import { resolve, dirname } from 'path'
 
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
