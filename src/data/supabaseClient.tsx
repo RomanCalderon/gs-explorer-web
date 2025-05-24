6a27c672b4cd4d662c4cb2aabfcd617dba3a47f1
RomanCalderon <romanc647@gmail.com>
2025-05-23 23:54:47 -0400

    Type assertion added for Supabase URL and key in supabaseClient.tsx

--- src/data/supabaseClient.tsx
+++ src/data/supabaseClient.tsx
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
